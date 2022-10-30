b6b778f6143f3a411dfb2399b0e5cd0cb57ba0b4
Artem04112 <artemsaakyan1@gmail.com>
2022-10-31 01:08:32 +0300

    Некоторые изменения

--- lab4/figures/circle/code.py
+++ lab4/figures/circle/code.py
@@ -1,9 +1,9 @@
 default_radius = 5
 def circle_perimeter(R = default_radius):
     print(f'circle_perimeter |{R}|')
-    P = 2 * 3.14 * R
-    print(P)
+    P_circle = 2 * 3.14 * R
+    return P_circle
 def circle_area(R = default_radius):
     print(f'circle_area |{R}|')
-    S = 3.14 * R*R
-    print(S)
+    S_circle = 3.14 * R * R
+    return S_circle
